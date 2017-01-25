ba68fb7656f5bb9e8507b2ca00ce4a145977b33a
Andy Boyle <andy@sudostream.io>
2017-01-25 09:57:03 +0000

    Remove generated comment

--- src/main/scala/io/sudostream/api_event_horizon/scriptwriter/business/ProcessSwaggerFileActor.scala
+++ src/main/scala/io/sudostream/api_event_horizon/scriptwriter/business/ProcessSwaggerFileActor.scala
@@ -3,9 +3,6 @@ package io.sudostream.api_event_horizon.scriptwriter.business
 import akka.actor.Actor
 import io.sudostream.api_event_horizon.scram.api.SwaggerJsonScramConverter
 
-/**
-  * Created by andy on 31/08/16.
-  */
 class ProcessSwaggerFileActor extends Actor {
 
   override def receive: Receive = {
