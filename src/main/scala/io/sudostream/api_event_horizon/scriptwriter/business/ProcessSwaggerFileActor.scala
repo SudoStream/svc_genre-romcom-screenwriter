d1f049bb7c361cac4414b9dec4ef7edb4fb38e2a
Andy Boyle <andy@sudostream.io>
2017-02-10 16:23:39 +0000

    minor renamings

--- src/main/scala/io/sudostream/api_event_horizon/scriptwriter/business/ProcessSwaggerFileActor.scala
+++ src/main/scala/io/sudostream/api_event_horizon/scriptwriter/business/ProcessSwaggerFileActor.scala
@@ -1,13 +1,13 @@
 package io.sudostream.api_event_horizon.scriptwriter.business
 
 import akka.actor.Actor
-import io.sudostream.api_event_horizon.scram.api.SwaggerJsonScreenplayWriterConverter
+import io.sudostream.api_event_horizon.scram.api.SwaggerJsonApiToScreenplayWriterConverter
 
 class ProcessSwaggerFileActor extends Actor {
 
   override def receive: Receive = {
     case swaggerSpec: String =>
-      val starBirthScram = new SwaggerJsonScreenplayWriterConverter().convertToScreenplayWriterAmateur(swaggerSpec).get
+      val starBirthScram = new SwaggerJsonApiToScreenplayWriterConverter().passApiDefinitionToScreenplayWriterAmateur(swaggerSpec).get
       sender ! starBirthScram.generateHappyPathTests
   }
 
